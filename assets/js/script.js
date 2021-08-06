f1352c4c19cf19cc960023dcb28e412861749ec3
hay-pingpong <83748758+hay-pingpong@users.noreply.github.com>
2021-08-06 23:20:46 +0900

    scroll animation ing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,48 +2,57 @@
   let yOffset = 0;
   const w = innerWidth;
   const h = innerHeight;
+  let basePoint;
 
   const sectionInfo = [
     {
-      // 0
-      type: "normal",
-      heightNum: 0,
-      scrollHeight: 0,
+      // home
       objs: {
         container: document.querySelector("#home"),
         visual: document.querySelector(".visual"),
         canvas: document.querySelector("#canvas"),
         context: canvas.getContext("2d"),
+        words: document.querySelectorAll("#home .word"),
+        word1: document.querySelector("#home .word1"),
+        word2: document.querySelector("#home .word2"),
+        word3: document.querySelector("#home .word3"),
       },
     },
     {
-      // 1
-      type: "normal",
-      heightNum: 0,
-      scrollHeight: 0,
+      // about
       objs: {
-        container: document.querySelector("#section1"),
+        container: document.querySelector("#about"),
+        arrows: document.querySelectorAll("#about .arrow"),
+        arrow1: document.querySelector("#about .arrow1"),
+        arrow2: document.querySelector("#about .arrow2"),
+        arrow3: document.querySelector("#about .arrow3"),
+
+        item1: document.querySelector("#about .item1"),
+        item2: document.querySelector("#about .item2"),
+        item3: document.querySelector("#about .item3"),
       },
     },
     {
-      // 2
-      type: "normal",
-      heightNum: 0,
-      scrollHeight: 0,
+      // works
       objs: {
-        container: document.querySelector("#section2"),
+        container: document.querySelector("#works"),
       },
     },
     {
-      // 3
-      type: "normal",
-      heightNum: 0,
-      scrollHeight: 0,
+      // hiring
       objs: {
-        container: document.querySelector("#section3"),
+        container: document.querySelector("#hiring"),
+      },
+    },
+    {
+      // contact
+      objs: {
+        container: document.querySelector("#contact"),
       },
     },
   ];
+
+  function setCanvasDraw() {}
 
   function setLayout() {
     for (let i; i < sectionInfo.length; i++) {
@@ -54,13 +63,45 @@
       ].objs.container.style.height = `${sectionInfo[i].scrollHeight}px`;
     }
 
-    function scrollLoop() {}
+    function animate() {
+      // home
+      if (yOffset + h > sectionInfo[0].objs.word1.offsetTop - h) {
+        sectionInfo[0].objs.words.forEach((word, i) => {
+          setTimeout(function () {
+            word.classList.add("on");
+          }, 100 * (i + 1));
+        });
+      }
+
+      // about
+      if (0 > sectionInfo[1].objs.item1.getBoundingClientRect().top - h) {
+        sectionInfo[1].objs.arrows.forEach((item) => {
+          item.classList.remove("on");
+        });
+        sectionInfo[1].objs.arrow1.classList.add("on");
+      }
+      if (0 > sectionInfo[1].objs.item2.getBoundingClientRect().top - h) {
+        sectionInfo[1].objs.arrows.forEach((item) => {
+          item.classList.remove("on");
+        });
+        sectionInfo[1].objs.arrow2.classList.add("on");
+      }
+      if (0 > sectionInfo[1].objs.item3.getBoundingClientRect().top - h) {
+        sectionInfo[1].objs.arrows.forEach((item) => {
+          item.classList.remove("on");
+        });
+        sectionInfo[1].objs.arrow3.classList.add("on");
+      }
+    }
 
     window.addEventListener("resize", setLayout);
 
     window.addEventListener("scroll", () => {
       yOffset = window.pageYOffset;
-      scrollLoop();
+      basePoint = yOffset + h / 2;
+      console.log("yOffset", yOffset);
+      // console.log("basePoint", basePoint);
+      animate();
     });
   }
 
@@ -69,5 +110,9 @@
     setLayout();
     sectionInfo[0].objs.context.fillStyle = "#B9FF7D";
     sectionInfo[0].objs.context.fillRect(0, 0, w, h);
+
+    console.log(sectionInfo[1].objs.item1.getBoundingClientRect().top);
+    console.log(sectionInfo[1].objs.item2.getBoundingClientRect().top);
+    console.log(sectionInfo[1].objs.item3.getBoundingClientRect().top);
   });
 })();
