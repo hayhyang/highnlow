fe193c33e95e8fd2fd6612ff092334f75d6defb1
hay-pingpong <83748758+hay-pingpong@users.noreply.github.com>
2022-02-22 12:27:22 +0900

    email.js 적용

---
+++ assets/js/email.js
@@ -0,0 +1,51 @@
+import { init, sendForm } from "@emailjs/browser";
+
+init("user_Awr6KtvDuysdUwnHwX2rq");
+
+const form = document.getElementById("form");
+const name = document.querySelector("#name");
+const organization = document.querySelector("#organization");
+const email = document.querySelector("#email");
+const mobile = document.querySelector("#mobile");
+const content = document.querySelector("#content");
+const agree = document.querySelector("#agree");
+const btn = document.getElementById("submit_button");
+
+const checkMobileFormat = (e) => {
+  const keyCode = e.keyCode;
+  if (keyCode > 58 || keyCode < 47) e.preventDefault();
+};
+
+const sendEmail = (e) => {
+  e.preventDefault();
+
+  if (!agree.checked) {
+    alert("개인정보 수집 이용 약관에 동의해주세요.");
+    return;
+  }
+
+  btn.value = "Sending...";
+
+  const templateID = "template_pj3h1uw";
+  const serviceID = "service_fwtevtb";
+
+  sendForm(serviceID, templateID, "#form").then(
+    () => {
+      btn.value = "문의 내용 전송하기";
+      alert("이메일 전송이 완료되었습니다.");
+      name.value = "";
+      organization.value = "";
+      email.value = "";
+      mobile.value = "";
+      content.value = "";
+      agree.checked = false;
+    },
+    (err) => {
+      btn.value = "문의 내용 전송하기";
+      alert(JSON.stringify(err));
+    }
+  );
+};
+
+form.addEventListener("submit", sendEmail);
+mobile.addEventListener("keypress", checkMobileFormat);
